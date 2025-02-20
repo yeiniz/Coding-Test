3558fdaa11ff3107737d91c717cb8d7b45a2b79f
Yein <78401825+yeiniz@users.noreply.github.com>
2025-02-20 15:57:26 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,13 @@
+a, b, c = map(int, input().split())
+
+if a==b and b==c:
+  print(10000+a*1000)
+elif a!=b and a!=c and b!=c:
+  print(max([a,b,c])*100)
+else:
+  if a==b:
+    print(1000+a*100)
+  elif a==c:
+    print(1000+a*100)
+  else:
+    print(1000+b*100)
